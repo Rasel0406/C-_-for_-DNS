1f04f1c933deae83e8823d828d7c620f3695385a
Rasl0406 <durjoychowdhury0406@gmail.comm>
2023-11-16 02:02:18 +0600

    teenatory operator

--- Module-01/ternatory_operator.cpp
+++ Module-01/ternatory_operator.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n ;
+    // if (n%2==0)
+    // {
+    //    cout << "Even Number" << endl;
+    // }
+    // else{
+    //     cout << "odd Number" << endl;
+    // }
+
+    (n%2==0)? cout << "Even Number" << endl : cout << "Odd Number" <<endl;
     return 0;
 }
